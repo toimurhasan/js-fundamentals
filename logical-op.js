8bb021e5a19d3abb410fbebed8f35c91abbb7488
toimurhasan <2imur.hasan@gmail.com>
2025-02-05 21:35:27 +0600

    added logical operators

---
+++ logical-op.js
@@ -0,0 +1,9 @@
+const salary = 10000;
+const isBCS = true;
+const hasBike = true;
+
+if ((salary >= 25000 || isBCS) && hasBike) {
+  console.log("the marriage is possible");
+} else {
+  console.log("go away");
+}
